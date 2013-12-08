9166e05b2b711f400300b7da9cb21564a424508d
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2013-12-08 14:14:15 +0100

    Details

--- src/ovski/minecraft/api/command/SubCommand.java
+++ src/ovski/minecraft/api/command/SubCommand.java
@@ -2,9 +2,23 @@ package ovski.minecraft.api.command;
 
 import org.bukkit.entity.Player;
 
+/**
+ * SubCommand
+ * 
+ * A sub command to lighten classes that implements commandExecutor
+ * 
+ * @author baptiste <baptiste.bouchereau@gmail.com>
+ */
 public abstract class SubCommand
 {
+    /**
+     * The player launching the command
+     */
     protected Player player;
+
+    /**
+     * The command arguments
+     */
     protected String[] args;
 
     protected SubCommand(Player player, String[] args)
